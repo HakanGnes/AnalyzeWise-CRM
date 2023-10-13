24dd6e88ac20344cfdbda702f72d68e65be912d1
HakanGnes <100923570+HakanGnes@users.noreply.github.com>
2023-10-13 10:58:15 +0300

    Crm

---
+++ Streamlit_Crm.py
@@ -0,0 +1,201 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from crm_Project import create_rfm, create_cltv_c, create_cltv_p,check_id,arl_recommender,rules,create_rules
+
+# Upload Data
+df = pd.read_excel("Data/Sales-Transaction-v.4a.xlsx")
+
+# Create DataFrames
+rfm_df = create_rfm(df)
+rfm_df.reset_index(inplace=True)
+cltv_p_df = create_cltv_p(df)
+rules = create_rules(df)
+cltv_c_df = create_cltv_c(df)
+# Sidebar choice Bar
+analysis_choice = st.sidebar.radio('Choose Analyze 👇', ('RFM Analyze','CLTV- Expected Purchase Prediction',"CLTV- Expected Profit and CLV Prediction",'CLTV-C Analyze',"Product Recommendation"))
+
+row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
+with row0_1:
+    st.title('AnalyzeWise CRM')
+with row0_2:
+    st.text("")
+    st.subheader('Streamlit App by [Hakan Gunes](https://www.linkedin.com/in/hakan-g%C3%BCne%C5%9F-a3b14720b/)')
+row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
+with row3_1:
+    st.markdown("In the world of retail, success hinges on mastering data. RetailGenius offers deep retail analytics to empower your business growth.")
+    st.markdown("📈 RFM Analysis: Understand customer behaviors and create personalized targeting using RFM analysis.")
+    st.markdown("💡 CLTV Analysis: Predict customer lifetime value and boost your profits. Identify which customers are the most valuable for your business.")
+    st.markdown("📊 Expected Purchase Values: Create robust sales forecasts to optimize inventory management and meet demand.")
+    st.markdown("💼 CLTV- Expected Profit and CLV Prediction: Refers to an analytical method and modeling process that helps predict customers' future profit potential and estimate their Customer Lifetime Value.")
+    st.markdown("📬 Product Recommendation: A recommendation system for the opportunity to cross-sell products that have been chosen together.")
+    st.markdown("You can find the source code in the [Hakan GitHub Repository](https://github.com/tdenzl/BuLiAn)")
+    st.write("Choose Analyze option from the sidebar.")
+
+
+if analysis_choice == 'RFM Analyze':
+    st.header('RFM Analyze')
+    # Segment selection interface
+    selected_segment = st.selectbox('Select Segment', rfm_df['segment'].unique())
+    # Filter by selected segment
+    filtered_customers = rfm_df[rfm_df['segment'] == selected_segment]['CustomerNo']
+    st.write(f"Customer numbers of {selected_segment} segment({len(filtered_customers)} customers):")
+    st.write(filtered_customers)
+
+    # Calculate the percentage of each segment relative to other segments
+    segment_counts = rfm_df['segment'].value_counts()
+    segment_percentages = segment_counts / segment_counts.sum() * 100
+
+    # Get percentage of selected segment
+    selected_segment_percentage = segment_percentages[selected_segment]
+
+    # Get percentage distribution of other segments (subtract selected segment)
+    other_segments_percentage = segment_percentages.drop(index=selected_segment)
+
+    # Create a pie chart showing the selected segment as a percentage distribution relative to other segments
+    labels = other_segments_percentage.index.tolist()
+    sizes = other_segments_percentage.values.tolist()
+    sizes.append(selected_segment_percentage)
+    labels.append(selected_segment)
+    explode = [0.1 if label == selected_segment else 0 for label in labels]  # Let the first slice separate slightly.
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+
+    st.subheader(f"Distribution of {selected_segment} segment compared to other segments")
+    st.pyplot(fig1)
+
+elif analysis_choice == 'CLTV- Expected Purchase Prediction':
+    st.header('CLTV- Expected Purchase Prediction')
+
+    # CLTV ve Segment seçme arayüzü
+    selected_segment = st.selectbox('Select Segment', cltv_p_df['segment'].unique())
+
+    # Seçilen segmente göre müşteri numarası seçme arayüzü
+    filtered_customers = cltv_p_df[cltv_p_df['segment'] == selected_segment]['CustomerNo']
+    selected_customer1 = st.selectbox('Select Customer Number 1', filtered_customers)
+
+    filtered_customers = cltv_p_df[cltv_p_df['segment'] == selected_segment]['CustomerNo']
+    selected_customer2 = st.selectbox('Select Customer Number 2', filtered_customers)
+
+    # Seçilen müşteri için beklenen satın alma değerlerini görüntüle - Display for the first customer
+    selected_customer_data1 = cltv_p_df[cltv_p_df['CustomerNo'] == selected_customer1]
+    st.write(f"{selected_customer1} customer's Expected Purchase Values:")
+    st.write("1 week: ", selected_customer_data1['expected_purc_1_week'].values[0])
+    st.write("1 month: ", selected_customer_data1['expected_purc_1_month'].values[0])
+    st.write("3 month: ", selected_customer_data1['expected_purc_3_month'].values[0])
+    st.write("6 month: ", selected_customer_data1['expected_purc_6_month'].values[0])
+    st.write("9 month: ", selected_customer_data1['expected_purc_9_month'].values[0])
+    st.write("12 month: ", selected_customer_data1['expected_purc_12_month'].values[0])
+
+    # Seçilen müşteri için beklenen satın alma değerlerini görüntüle - Display for the second customer
+    selected_customer_data2 = cltv_p_df[cltv_p_df['CustomerNo'] == selected_customer2]
+    st.write(f"{selected_customer2} customer's Expected Purchase Values:")
+    st.write("1 week: ", selected_customer_data2['expected_purc_1_week'].values[0])
+    st.write("1 month: ", selected_customer_data2['expected_purc_1_month'].values[0])
+    st.write("3 month: ", selected_customer_data2['expected_purc_3_month'].values[0])
+    st.write("6 month: ", selected_customer_data2['expected_purc_6_month'].values[0])
+    st.write("9 month: ", selected_customer_data2['expected_purc_9_month'].values[0])
+    st.write("12 month: ", selected_customer_data2['expected_purc_12_month'].values[0])
+
+    comparison_data = pd.DataFrame({
+        'Months': ['1 Week', '1 Month', '3 Months', '6 Months', '9 Months', '12 Months'],
+        f'{selected_customer1}': [
+            selected_customer_data1['expected_purc_1_week'].values[0],
+            selected_customer_data1['expected_purc_1_month'].values[0],
+            selected_customer_data1['expected_purc_3_month'].values[0],
+            selected_customer_data1['expected_purc_6_month'].values[0],
+            selected_customer_data1['expected_purc_9_month'].values[0],
+            selected_customer_data1['expected_purc_12_month'].values[0],
+        ],
+        f'{selected_customer2}': [
+            selected_customer_data2['expected_purc_1_week'].values[0],
+            selected_customer_data2['expected_purc_1_month'].values[0],
+            selected_customer_data2['expected_purc_3_month'].values[0],
+            selected_customer_data2['expected_purc_6_month'].values[0],
+            selected_customer_data2['expected_purc_9_month'].values[0],
+            selected_customer_data2['expected_purc_12_month'].values[0],
+        ]
+    })
+
+
+    # Add the average values
+    comparison_data['Average'] = comparison_data.iloc[:, 1:].mean(axis=1)
+
+    # Sort the DataFrame by the 'Average' column
+    comparison_data = comparison_data.sort_values(by='Average')
+
+    # Create a bar chart to compare the two customers for each month
+    st.bar_chart(comparison_data.set_index('Months'))
+
+elif analysis_choice == 'CLTV- Expected Profit and CLV Prediction':
+    st.header('CLTV- Expected Profit and CLV Prediction')
+
+    # Select Segment
+    selected_segment = st.selectbox('Select Segment', cltv_p_df['segment'].unique())
+
+    # Customer number selection interface according to the selected segment
+    filtered_customers = cltv_p_df[cltv_p_df['segment'] == selected_segment]['CustomerNo']
+    selected_customer1 = st.selectbox('Select Customer Number 1', filtered_customers)
+
+    filtered_customers = cltv_p_df[cltv_p_df['segment'] == selected_segment]['CustomerNo']
+    selected_customer2 = st.selectbox('Select Customer Number 2', filtered_customers)
+
+    # Display for the first customer
+    selected_customer_data1 = cltv_p_df[cltv_p_df['CustomerNo'] == selected_customer1]
+    st.write(f"{selected_customer2} customer's Information:")
+    st.write("Expected Average Profit: ", selected_customer_data1['expected_average_profit'].values[0])
+    st.write("CLV (Customer Lifetime Value): ", selected_customer_data1['clv'].values[0])
+    st.write("Segment: ", selected_customer_data1['segment'].values[0])
+
+    # Display for the second customer
+    selected_customer_data2 = cltv_p_df[cltv_p_df['CustomerNo'] == selected_customer2]
+    st.write(f"{selected_customer2} customer's Information:")
+    st.write("Expected Average Profit: ", selected_customer_data2['expected_average_profit'].values[0])
+    st.write("CLV (Customer Lifetime Value): ", selected_customer_data2['clv'].values[0])
+    st.write("Segment: ", selected_customer_data2['segment'].values[0])
+
+elif analysis_choice == 'CLTV-C Analyze':
+    st.header('CLTV-C Analyze')
+
+    # Select Segment
+    selected_segment = st.selectbox('Select Segment', cltv_p_df['segment'].unique())
+
+    # Customer number selection interface according to the selected segment
+    filtered_customers = cltv_p_df[cltv_p_df['segment'] == selected_segment]['CustomerNo']
+    selected_customer = st.selectbox('Select Customer Number', filtered_customers)
+
+    # View CLTV-C value of selected customer
+    selected_customer_data = cltv_c_df.loc[selected_customer]
+    st.write(f"{selected_customer} customer's CLTV-C Analysis:")
+    st.write("Total Number of Transactions: ", selected_customer_data['total_transaction'])
+    st.write("Total Number of Products: ", selected_customer_data['total_unit'])
+    st.write("Total amount: ", selected_customer_data['total_price'])
+    st.write("Average Order Value: ", selected_customer_data['avg_order_value'])
+    st.write("Purchase Frequency: ", selected_customer_data['purchase_frequency'])
+    st.write("Profit margin: ", selected_customer_data['profit_margin'])
+    st.write("Customer Value: ", selected_customer_data['customer_value'])
+    st.write("CLTV-C Value: ", selected_customer_data['cltv'])
+    st.write("Segment: ", selected_customer_data['segment'])
+
+    # Create a bar chart to compare the selected customer's CLTV-C value with the average CLTV-C value
+    customer_cltv = selected_customer_data['cltv']
+    avg_cltv = cltv_c_df['cltv'].mean()
+    st.bar_chart({'Selected Customer': customer_cltv, 'Average CLTV': avg_cltv})
+
+
+elif analysis_choice == 'Product Recommendation':
+    st.header('Product Recommendations')
+    selected_product = st.selectbox('Select Product', df['ProductNo'].unique())
+    recommendation_count = st.slider('How Many Product Recommendations Should Be Made?', 1, 10, 5)
+
+    if st.button('View Recommendations'):
+        recommendations = arl_recommender(rules, selected_product, recommendation_count)
+        st.write(f"{selected_product} for recommended products :")
+        for i, recommendation in enumerate(recommendations, start=1):
+            st.write(f"{i}. Recommendation: {recommendation}")
+
+
+
---
+++ crm_Project.py
@@ -0,0 +1,243 @@
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
+from mlxtend.frequent_patterns import apriori, association_rules
+
+df_ = pd.read_excel("Data/Sales-Transaction-v.4a.xlsx")
+df = df_.copy()
+df.head()
+df.shape
+# df = df[~df["TransactionNo"].str.contains("C", na=False)]
+# df.info()
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.25)
+    quartile3 = dataframe[variable].quantile(0.75)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["TransactionNo"].str.contains("C", na=False)]
+    dataframe['Date'] = pd.to_datetime(dataframe['Date'], format='%m/%d/%Y')
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+# df = retail_data_prep(df)
+today_date = dt.datetime(2019, 12, 11)
+def create_rfm(dataframe, csv=False):
+    # VERIYI HAZIRLAMA
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe['Date'] = pd.to_datetime(dataframe['Date'], format='%m/%d/%Y')
+    dataframe = dataframe[~dataframe["TransactionNo"].str.contains("C", na=False)]
+
+    # RFM METRIKLERININ HESAPLANMASI
+    today_date = dt.datetime(2019, 12, 11)
+    rfm = dataframe.groupby('CustomerNo').agg({'Date': lambda date: (today_date - date.max()).days,
+                                               'TransactionNo': lambda num: num.nunique(),
+                                               "TotalPrice": lambda price: price.sum()})
+    rfm.columns = ['recency', 'frequency', "monetary"]
+
+    # RFM SKORLARININ HESAPLANMASI
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+    # cltv_df skorları kategorik değere dönüştürülüp df'e eklendi
+    rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                        rfm['frequency_score'].astype(str))
+
+    # SEGMENTLERIN ISIMLENDIRILMESI
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+
+    rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+    rfm.index = rfm.index.astype(int)
+
+    return rfm
+
+def create_cltv_c(dataframe, profit=0.10, csv=False):
+    # VERIYI HAZIRLAMA
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe['Date'] = pd.to_datetime(dataframe['Date'], format='%m/%d/%Y')
+    dataframe = dataframe[~dataframe["TransactionNo"].str.contains("C", na=False)]
+
+    cltv_c = dataframe.groupby('CustomerNo').agg({'TransactionNo': lambda x: x.nunique(),
+                                                  'Quantity': lambda x: x.sum(),
+                                                  "TotalPrice": lambda x: x.sum()})
+
+    cltv_c.columns = ['total_transaction', 'total_unit', 'total_price']
+    # avg_order_value
+    cltv_c['avg_order_value'] = cltv_c['total_price'] / cltv_c['total_transaction']
+    # purchase_frequency
+    cltv_c["purchase_frequency"] = cltv_c['total_transaction'] / cltv_c.shape[0]
+    # repeat rate & churn rate
+    repeat_rate = cltv_c[cltv_c.total_transaction > 1].shape[0] / cltv_c.shape[0]
+    churn_rate = 1 - repeat_rate
+    # profit_margin
+    cltv_c['profit_margin'] = cltv_c['total_price'] * profit
+    # Customer Value
+    cltv_c['customer_value'] = (cltv_c['avg_order_value'] * cltv_c["purchase_frequency"])
+    # Customer Lifetime Value
+    cltv_c['cltv'] = (cltv_c['customer_value'] / churn_rate) * cltv_c['profit_margin']
+    # Segment
+    cltv_c["segment"] = pd.qcut(cltv_c["cltv"], 4, labels=["low", "mid", "high", "very high"])
+
+    return cltv_c
+
+
+def create_cltv_p(dataframe, month=3, csv=False):
+    # 1. Veri Ön İşleme
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    dataframe['Date'] = pd.to_datetime(dataframe['Date'], format='%m/%d/%Y')
+    dataframe = dataframe[~dataframe["TransactionNo"].str.contains("C", na=False)]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    today_date = dt.datetime(2019, 12, 11)
+
+    cltv_df = dataframe.groupby('CustomerNo').agg(
+        {'Date': [lambda Date: (Date.max() - Date.min()).days,
+                  lambda Date: (today_date - Date.min()).days],
+         'TransactionNo': lambda TransactionNo: TransactionNo.nunique(),
+         'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    cltv_df.columns = cltv_df.columns.droplevel(0)
+    cltv_df.columns = ['recency', 'T', 'frequency', 'monetary']
+    cltv_df["monetary"] = cltv_df["monetary"] / cltv_df["frequency"]
+    cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+    cltv_df["recency"] = cltv_df["recency"] / 7
+    cltv_df["T"] = cltv_df["T"] / 7
+
+    # 2. BG-NBD Modelinin Kurulması
+    bgf = BetaGeoFitter(penalizer_coef=0.001)
+    bgf.fit(cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T'])
+
+    cltv_df["expected_purc_1_week"] = bgf.predict(1,
+                                                  cltv_df['frequency'],
+                                                  cltv_df['recency'],
+                                                  cltv_df['T'])
+
+    cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_3_month"] = bgf.predict(12,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_6_month"] = bgf.predict(26,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_9_month"] = bgf.predict(39,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_12_month"] = bgf.predict(52,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+
+    # 3. GAMMA-GAMMA Modelinin Kurulması
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(cltv_df['frequency'], cltv_df['monetary'])
+    cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                                 cltv_df['monetary'])
+
+    # 4. BG-NBD ve GG modeli ile CLTV'nin hesaplanması.
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency'],
+                                       cltv_df['T'],
+                                       cltv_df['monetary'],
+                                       time=month,  # 3 aylık
+                                       freq="W",  # T'nin frekans bilgisi.
+                                       discount_rate=0.01)
+
+    cltv = cltv.reset_index()
+    cltv_final = cltv_df.merge(cltv, on="CustomerNo", how="left")
+    cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["low", "mid", "high", "very high"])
+
+    return cltv_final
+
+# RECOMENNDER SYSTEM (ARL)
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(['TransactionNo', "ProductNo"])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(['TransactionNo', 'ProductName'])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["ProductNo"] == stock_code][["ProductName"]].values[0].tolist()
+    print(product_name)
+def create_rules(dataframe, id=True, country="France"):
+    dataframe = dataframe[dataframe['Country'] == country]
+    dataframe = create_invoice_product_df(dataframe, id)
+    frequent_itemsets = apriori(dataframe, min_support=0.01, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+    return rules
+
+rules = create_rules(df)
+
+rules[(rules["support"] > 0.05) & (rules["confidence"] > 0.1) & (rules["lift"] > 5)]. \
+    sort_values("confidence", ascending=False)
+
+def arl_recommender(rules_df, product_id, rec_count=1):
+     sorted_rules = rules_df.sort_values("lift", ascending=False)
+     recommendation_list = []
+     for i, product in enumerate(sorted_rules["antecedents"]):
+        for j in list(product):
+           if j == product_id:
+                recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+
+     return recommendation_list[0:rec_count]
+
+
+arl_recommender(rules, 21080, 5)
+
+

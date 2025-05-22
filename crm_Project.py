809790f6d0a11062c285ed1ac92e567c4146b206
Hakan Gunes <100923570+HakanGnes@users.noreply.github.com>
2025-05-22 19:15:47 +0300

    Update crm_Project.py

--- crm_Project.py
+++ crm_Project.py
@@ -67,17 +67,17 @@ def create_rfm(dataframe, csv=False):
                         rfm['frequency_score'].astype(str))
 
     # SEGMENTLERIN ISIMLENDIRILMESI
-    seg_map = {
-        r'[1-2][1-2]': 'hibernating',
-        r'[1-2][3-4]': 'at_risk',
-        r'[1-2]5': 'cant_loose',
-        r'3[1-2]': 'about_to_sleep',
-        r'33': 'need_attention',
-        r'[3-4][4-5]': 'loyal_customers',
-        r'41': 'promising',
-        r'51': 'new_customers',
-        r'[4-5][2-3]': 'potential_loyalists',
-        r'5[4-5]': 'champions'
+    seg_map_tr = {
+    r'[1-2][1-2]': 'kış uykusundakiler',          # hibernating
+    r'[1-2][3-4]': 'riskte olanlar',              # at_risk
+    r'[1-2]5'    : 'kaybedilmemeli',              # cant_loose
+    r'3[1-2]'    : 'uykuya yakın',                # about_to_sleep
+    r'33'        : 'ilgi gerek',                  # need_attention
+    r'[3-4][4-5]': 'sadık müşteriler',            # loyal_customers
+    r'41'        : 'umut veren',                  # promising
+    r'51'        : 'yeni müşteriler',             # new_customers
+    r'[4-5][2-3]': 'potansiyel sadıklar',         # potential_loyalists
+    r'5[4-5]'    : 'şampiyonlar'                  # champions
     }
 
     rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
